46eabbf73c2d7b8c728bf5e1d1f2c32f5b6ea3f3
Patrick Connolly <patrick.c.connolly@gmail.com>
2019-04-05 00:03:55 -0400

    Added grid format option for embed widget.

--- embed.js
+++ embed.js
@@ -5,6 +5,7 @@ const app = new Vue({
   data: {
     rows: [],
     statusFilter: '',
+    style: 'grid',
   },
   // NOTE: We do this because both Jekyll and VueJS try to use
   // double-curly-bracket syntax, eg. {{ some_var }}
@@ -23,6 +24,7 @@ const app = new Vue({
       const key = window.frameElement.getAttribute('data-gsheet-key') || '{{ site.gsheet.key }}'
       const id = window.frameElement.getAttribute('data-gsheet-sheet-id') || '{{ site.gsheet.sheet_id }}'
       vm.statusFilter = window.frameElement.getAttribute('data-status') || ''
+      vm.style = window.frameElement.getAttribute('data-style') || 'grid'
       const csvUrl = `https://docs.google.com/spreadsheets/d/${key}/export?format=csv&id=${key}&gid=${id}`
       Papa.parse(csvUrl, {
         download: true,
